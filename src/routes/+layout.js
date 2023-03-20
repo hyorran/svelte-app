8466aac052859f3aedf5c6335bfe26cff2dfd2ad
Hyorran <phyorran@gmail.com>
2023-03-20 20:26:48 +0000

    store default value equals to local data

---
+++ src/routes/+layout.js
@@ -0,0 +1,8 @@
+export async function load({ fetch }) {
+	const response = await fetch(`../data.json`); // stored in static folder
+	const posts = await response.json();
+
+	return {
+		props: { posts }
+	};
+}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script>
-
   import "../app.css";
   import NavigationBar from "../components/NavigationBar.svelte";
+  import { ReviewsStore } from "../Store.js";
+
+  export let data;
+
+  $ReviewsStore = data.props.posts;
 </script>
 
 <NavigationBar />
