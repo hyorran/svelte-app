a7175867774a291c94e6091b43834e2ef83982d1
Hyorran <phyorran@gmail.com>
2023-03-20 20:13:03 +0000

    created store to store reviews data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,15 @@
 <script>
   import ReviewsList from "../components/ReviewsList/ReviewsList.svelte";
-
-  export let data;
-
+  import { ReviewsStore } from "../Store.js";
 </script>
 
 
 <main class="md:container md:mx-auto">
-  <ReviewsList draggable={false} reviews={data.props.posts.toplists['575'].sort((a, b) => a.position - b.position)} />
+  <ReviewsList
+    draggable={false}
+    onDrop={() => null}
+    reviews={$ReviewsStore.toplists['575'].sort((a, b) => a.position - b.position)}
+  />
 </main>
 
 <style lang="postcss">
--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,10 +1,10 @@
 <script>
   import ReviewsList from "../../components/ReviewsList/ReviewsList.svelte";
+  import { ReviewsStore } from "../../Store.js";
 
-  export let data;
   let reviews;
 
-  function handleSubmit(newReviews) {
+  function onDrop(newReviews) {
     if (newReviews) {
       reviews = newReviews.map((review, index) => {
         return {
@@ -14,18 +14,22 @@
       });
     }
   }
+
+  function handleSubmit(newData) {
+    $ReviewsStore.toplists["575"] = newData;
+  }
 </script>
 
 
 <main class="md:container md:mx-auto">
   <ReviewsList
     draggable
-    onDrop={handleSubmit}
-    reviews={data.props.posts.toplists['575'].sort((a, b) => a.position - b.position)}
+    onDrop={onDrop}
+    reviews={$ReviewsStore.toplists['575'].sort((a, b) => a.position - b.position)}
   />
   <button
     class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-    on:click|preventDefault={handleSubmit}
+    on:click|preventDefault={handleSubmit(reviews)}
     type="button"
   >
     Save
